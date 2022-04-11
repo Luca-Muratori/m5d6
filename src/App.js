138d553808105d9803f8357fcffc6e1de230a124
Lucai994 <luca.muratori.94@hotmail.it>
2022-04-11 16:47:59 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    blogs: [],
+    authors: [],
+  };
+  render() {
+    return (
+      <div className="App">
+        <div id="authors">
+          <div>Authors in the catalogue</div>
+          {this.state.authors.map((author, index) => {
+            <div key={index}>
+              <p>{author.name}</p>
+            </div>;
+          })}
+        </div>
+        <div id="blogs">
+          <div>blogs in the catalogue</div>
+          {this.state.blogs.map((blog, index) => {
+            <div key={index}>
+              <p>{blog.name}</p>
+            </div>;
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount = async () => {
+    const apiUrl = process.env.REACT_APP_URL;
+    const respAuthors = await fetch(`${apiUrl}/authors`);
+    const respBlogs = await fetch(`${apiUrl}/blogs`);
+
+    const jsonAuthors = await respAuthors.json();
+    this.setState({ authors: jsonAuthors });
+    const jsonBlogs = await respBlogs.json();
+    this.setState({ blogs: jsonBlogs });
+  };
 }
 
 export default App;
